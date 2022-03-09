9675d4e2f47727bcd5bb67bdc55db28fcbb70e79
GiulioAntoni <giulio.antoni03@gmail.com>
2022-03-09 17:50:45 +0100

    eliminazione celle

--- s7/g1/js/pagina.js
+++ s7/g1/js/pagina.js
@@ -15,17 +15,34 @@ dataBase.forEach(function (elemento) {
     let celle1 = document.createElement('td');
     let celle2 = document.createElement('td');
     let celle3 = document.createElement('td');
+    let celle4 = document.createElement('button')
 
     let contenitore = document.querySelector('#tabella')
-
 
     celle1.innerText = user
     celle2.innerText = email
     celle3.innerText = pass
+    celle4.innerText= 'elimina'
 
     contenitore.append(riga)
-    riga.append(celle1, celle2, celle3) 
+    riga.append(celle1, celle2, celle3,celle4) 
 
-
-
+    celle4.addEventListener('click',function(){
+        riga.remove();
+        elimina(riga)
+    })
 })
+
+
+function elimina(elemento) {
+    let elementiSalvati = localStorage.getItem('lista')
+    let dataBase = elementiSalvati == null ? [] : JSON.parse(elementiSalvati)
+
+    var focus = dataBase.indexOf(elemento)
+    dataBase.splice(focus, 1)
+
+    localStorage.setItem('lista', JSON.stringify(dataBase));
+    
+}
+
+
